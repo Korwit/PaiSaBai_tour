0ba3484369cd6a113169ee04f58f2b8bddd40ed6
Boonyarit <boonyaritbuasom47@gmail.com>
2024-03-06 01:48:55 +0700

    Change  hs and ps Page

--- frontend/src/page/paymentstatus.js
+++ frontend/src/page/paymentstatus.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import NavBar from "../component/navbar-main";
 import PaymentStatusCard from "../component/card-paymentstatus";
 import '../css/paymentstatus.css'
