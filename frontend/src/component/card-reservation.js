995da1eef5500ee3b79a1a3656eb3e1a1c673d31
Sukhita Liaopatthanapong <sukhita5926@gmail.com>
2024-03-06 04:33:20 +0700

    edit

--- frontend/src/component/card-reservation.js
+++ frontend/src/component/card-reservation.js
@@ -4,9 +4,11 @@ import { useNavigate } from 'react-router-dom';
 import Select from 'react-select';
 import axios from 'axios';
 import { setDate } from 'rsuite/esm/utils/dateUtils';
+import { useParams } from 'react-router-dom';
 
 
 const ReservationCard = ({data}) => {
+    const { cardId } = useParams();
     const navigate = useNavigate();
     const [firstName, setFirstname] = useState('');
     const [lastName, setLastname] = useState('');
@@ -18,9 +20,11 @@ const ReservationCard = ({data}) => {
     const [price, setPrice] = useState(0);
     const jwt = localStorage.getItem('jwt')
     axios.defaults.headers.common = { 'Authorization': `bearer ${jwt}` }
+    console.log(cardId)
     console.log(data)
     //const { tourId } = useParams();
     useEffect(() => {
+        
         // ทำการดึงข้อมูลจาก Strapi
         //console.log(data.id)
         const fetchData = async () => {
@@ -31,7 +35,7 @@ const ReservationCard = ({data}) => {
                 setLastname(userData.data.lastname);
                 setPhone(userData.data.phone);
         
-                const tourData = await axios.get(`/tours/${data.tour.id}`);
+                const tourData = await axios.get(`/tours/${cardId}`);
                 const formattedOptions = tourData.data.map((item) => ({
                   label: item.label,
                   value: item.value,
