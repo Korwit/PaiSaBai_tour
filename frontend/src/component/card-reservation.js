13b3e4af2d4c62cec9b22028bd464e238d243acb
กรวิทย์ กอหลัง <kornwit6404@gmail.com>
2024-03-06 07:11:57 +0700

    merge

--- frontend/src/component/card-reservation.js
+++ frontend/src/component/card-reservation.js
@@ -5,14 +5,10 @@ import Select from 'react-select';
 import axios from 'axios';
 import { setDate } from 'rsuite/esm/utils/dateUtils';
 import { useParams } from 'react-router-dom';
+import '../css/reservation.css';
 
 
-<<<<<<< HEAD
 const ReservationCard = ({ data }) => {
-=======
-const ReservationCard = ({data}) => {
-    const [name, setName] = useState('');
->>>>>>> 479bc31c5c9d6467986ea65f5d5a2cfb749cd4bf
     const { cardId } = useParams();
     const navigate = useNavigate();
     const [firstName, setFirstname] = useState('');
@@ -23,21 +19,11 @@ const ReservationCard = ({data}) => {
     const [options, setOptions] = useState([]);
     const [selectedOption, setSelectedOption] = useState([]);
     const [price, setPrice] = useState(0);
-<<<<<<< HEAD
     const [datas, setdata] = useState('');
     const [owners, setowners] = useState('');
     const jwt = localStorage.getItem('jwt')
     axios.defaults.headers.common = { 'Authorization': `bearer ${jwt}` }
 
-=======
-
-    const jwt = localStorage.getItem('jwt')
-    axios.defaults.headers.common = { 'Authorization': `bearer ${jwt}` }
-    console.log(cardId)
-    
-    console.log(data)
-    //const { tourId } = useParams();
->>>>>>> 479bc31c5c9d6467986ea65f5d5a2cfb749cd4bf
     useEffect(() => {
 
         // ทำการดึงข้อมูลจาก Strapi
@@ -48,7 +34,6 @@ const ReservationCard = ({data}) => {
                 setFirstname(userData.data.firstname);
                 setLastname(userData.data.lastname);
                 setPhone(userData.data.phone);
-<<<<<<< HEAD
                 setowners(userData.data.id);
                 const tourData = await axios.get(`/tours/${cardId}?populate=*`);
                 setdata(tourData.data.data)                
@@ -61,20 +46,6 @@ const ReservationCard = ({data}) => {
                 setSelectedOption(r[0]);
 
             } catch (error) {
-=======
-                const tourData = await axios.get(`/tours/${cardId}?populate=*`);
-
-                console.log(tourData)
-                setName(tourData.data.name);
-                setPrice(tourData.data.price);
-                console.log(price)
-                const formattedOptions = tourData.data.trip_date.map((item) => ({
-                  label: item.label,
-                  value: item.value,
-                }));
-                setOptions(formattedOptions);
-              } catch (error) {
->>>>>>> 479bc31c5c9d6467986ea65f5d5a2cfb749cd4bf
                 console.error('Error fetching data:', error);
             }
         };
@@ -91,7 +62,6 @@ const ReservationCard = ({data}) => {
     const handleSubmit = async (e) => {
        console.log(datas.attributes.id)
         try {
-<<<<<<< HEAD
            
             await axios.post('/reservations', {
                 data:
@@ -101,15 +71,6 @@ const ReservationCard = ({data}) => {
                 }
             });
             navigate("/payment")
-=======
-            // ทำ HTTP request ไปยัง Strapi API โดยใช้ axios
-            setIsLoading(true)
-            await axios.post('http://localhost:1337/api/auth/local/reservation', {
-                
-                selectedOption: selectedOption?.value || null, 
-        });
-        
->>>>>>> 479bc31c5c9d6467986ea65f5d5a2cfb749cd4bf
         } catch (error) {
             console.error('Error sending data to Strapi:', error);
 
@@ -130,10 +91,11 @@ const ReservationCard = ({data}) => {
     }
 
     return (
+        <div>
         <form onSubmit={handleSubmit}>
             <h2>ข้อมูลผู้เข้าจอง</h2>
             <h4>กรุณากรอกข้อมูลและตรวจสอบการจอง</h4>
-            <h3>โปรแกรมทัวร์</h3>{name}
+            <h3>โปรแกรมทัวร์</h3>
             <Form.Label>วันที่เดินทาง :</Form.Label>
             <Select
                 options={options}
@@ -148,26 +110,17 @@ const ReservationCard = ({data}) => {
             <React.Fragment>
                 <strong>ชื่อ :</strong> {firstName} <strong>นามสกุล :</strong> {lastName}<br />
                 <strong>เบอร์โทรศัพท์ :</strong> {phone} <br />
-<<<<<<< HEAD
                 <strong>ราคา:</strong> {datas.attributes?.price} ฿<br />
             </React.Fragment>
             <Button
                 className='sm-cl1-pri'
                 type="button"
                 onClick={() => handleSubmit()}            >
-=======
-                <strong>ราคา:</strong> {price} ฿<br />
-            </React.Fragment><br />
-            <center><Button
-                className='sm-cl1-pri' 
-                type="submit"
-                onClick={() => handleSubmit()}
-            >
->>>>>>> 479bc31c5c9d6467986ea65f5d5a2cfb749cd4bf
                 จอง
             </Button>&nbsp;&nbsp;&nbsp;&nbsp;
-            <Button className='sm-cl-sec' type="button" onClick={() => handlemain()}>ยกเลิก</Button></center>
-        </form>
+            <Button className='sm-cl-sec' type="button" onClick={() => handlemain()}>ยกเลิก</Button>
+            </form>
+            </div>
     );
 };
 
